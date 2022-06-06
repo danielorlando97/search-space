73394fe1643cc8854a53b8aa5223f6a183df73d6
danielorlando97 <danielorlandoortizpacheco@gmail.com>
2022-06-05 22:12:59 -0400

    update definition

--- src/class-api/protocols.py
+++ src/class-api/protocols.py
@@ -1,20 +1,4 @@
-from typing import Protocol
-
-class Sampler(Protocol):
-    """iterator by its probability distribution, defined domain"""
-
-class Domain(Protocol):
-    """context in defined a sampler"""
-
-class FiniteDomain(Domain):
-    """In this context the options are numerable"""
-
-class InfiniteDomain(Domain):
-    """In this context the options aren't numerable"""
-
-class SearchSpace(Protocol):
-    """Is the union between one domain and one sampling"""
-
+from typing import Any, Protocol, TypeVar, Union, List
 
 """
 SearchSpace = Object's set
@@ -22,3 +6,25 @@ Sampling = get an object of set
 Optimal Sampling = get the best object of set by objective function  
 Valid Sampling = get an object of set that it complies with all rules of the describe
 """
+T = TypeVar('T')
+
+class Constraint(Protocol):
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        pass
+
+class SearchSpace(Protocol[T]):
+    """Is a defined and iterable domain"""
+
+    def __or__(self, c: Constraint) -> 'SearchSpace[T]':
+        pass
+
+    def __iter__(self):
+        pass
+
+class FiniteSearchSpace(SearchSpace[T], Protocol[T]):
+    def __len__(self) -> int:
+        pass
+
+class OrdinalSearchSpace(SearchSpace[T], Protocol[T]):
+    def __getitem__(self, index: int) -> T:
+        pass
