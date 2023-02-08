74e35b67e6b243088c405c1842b8146f5906c21c
danielorlando97 <danielorlandoortizpacheco@gmail.com>
2023-02-07 21:33:28 -0500

    refactor class spaces

--- search_space/spaces/build_in_spaces/categorical_search_space.py
+++ search_space/spaces/build_in_spaces/categorical_search_space.py
@@ -20,33 +20,6 @@ class BasicCategoricalSearchSpace(BasicSearchSpace):
             path=path
         )
 
-    # def __advance_space__(self, ast):
-
-    #     advance_space = CategoricalSearchSpace(
-    #         self.initial_domain, distribute_like=self.__distribute_like__)
-
-    #     advance_space.ast_constraint = ast
-    #     return advance_space
-
-
-# class CategoricalSearchSpace(SearchSpace):
-#     def __init__(self, domain, distribute_like=UNIFORM) -> None:
-#         super().__init__(domain, distribute_like=distribute_like)
-#         self.visitor_layers.append(visitors.DomainModifierVisitor())
-
-#     def __sampler__(self, domain, context):
-#         return domain.get_sample(self._distribution), context
-
-#     def __domain_filter__(self, domain, context):
-#         c_domain = CategoricalDomain(domain)
-#         return super().__domain_filter__(c_domain, context)
-
-#     def __copy__(self):
-#         clone = CategoricalSearchSpace(*self.initial_domain)
-#         clone.set_sampler(SamplerFactory().create_sampler(
-#             self._distribution.__distribute_name__, search_space=clone))
-#         return clone
-
 
 @SpacesManager.registry(bool)
 class BasicBooleanSearchSpace(BasicSearchSpace):
@@ -60,21 +33,3 @@ class BasicBooleanSearchSpace(BasicSearchSpace):
             distribute_like=distribute_like,
             path=path
         )
-
-    # def __advance_space__(self, ast):
-    #     advance_space = BooleanSearchSpace(
-    #         self.initial_domain, distribute_like=self.__distribute_like__)
-
-    #     advance_space.ast_constraint = ast
-    #     return advance_space
-
-
-# class BooleanSearchSpace(CategoricalSearchSpace):
-#     def __init__(self, domain, distribute_like=UNIFORM) -> None:
-#         super().__init__(domain, distribute_like=distribute_like)
-
-#     def __copy__(self):
-#         clone = BooleanSearchSpace()
-#         clone.set_sampler(SamplerFactory().create_sampler(
-#             self._distribution.__distribute_name__, search_space=clone))
-#         return clone
--- search_space/spaces/build_in_spaces/space_manager.py
+++ search_space/spaces/build_in_spaces/space_manager.py
@@ -1,14 +1,13 @@
 from copy import copy
 import inspect
 from typing_extensions import Self
-from search_space.context_manager.sampler_context import SamplerContext
-from search_space.sampler.distribution_names import UNIFORM
+from search_space.sampler import BERNOULLI
 from search_space.spaces.visitors import visitors
 from search_space.spaces import BasicSearchSpace
 import imp
-from typing import List, Type
+from typing import Any, List, Type, Callable, Dict
 from search_space.utils.singleton import Singleton
-from search_space.spaces import SearchSpace
+from search_space.spaces.search_space import SearchSpace, GetSampleParameters, GetSampleResult
 from search_space.spaces.asts import constraints as ast_constraint
 from typing import _UnionGenericAlias
 from search_space.spaces.domains.categorical_domain import CategoricalDomain
@@ -76,7 +75,7 @@ class FunctionParamInfo:
     def __eq__(self, __o: object) -> bool:
         return __o == self.name
 
-    def get_sample(self, context=None, sub_space=[]):
+    def get_sample(self, params: GetSampleParameters = None, sub_space=[], path=None):
         if self.default is None:
             return None
 
@@ -86,26 +85,47 @@ class FunctionParamInfo:
             return self.default
 
         try:
-            value = [s for s in sub_space if hash(
-                s) == hash(self.default)][0]
+            value = next(filter(
+                lambda s: hash(s) == hash(self.default),
+                sub_space
+            ))
+
             name = value.space_name
+            path_space = value.path_space
             value.space_name = self.default.space_name
-        except IndexError:
-            name = self.default.space_name
+        except StopIteration:
             value = self.default
 
-        result, _ = value.get_sample(context=context)
+            name = value.space_name
+            path_space = value.path_space
+            value.path_space = path_space if path is None else path
+
+        result = value.get_sample(params)
+
+        value.path_space = path_space
         value.space_name = name
-        return result
+
+        return result.sample
 
 
 class ClassFunction:
-    def __init__(self, func, sub_space: list, _self, context=None, params=None, instance=None) -> None:
+    def __init__(
+        self,
+        func: Callable,
+        sub_space: List[BasicSearchSpace],
+        _self: Type,
+        sampler_params: GetSampleParameters = None,
+        params=None,
+        instance=None,
+        class_path=None
+    ) -> None:
+
         self.func = func
         self.params = params
-        self.context = context
+        self.sampler_params = sampler_params
         self.sub_space = []
         self.instance = instance
+        self.class_path = class_path
 
         if params is None:
             func_data = inspect.getfullargspec(func)
@@ -133,10 +153,12 @@ class ClassFunction:
                     _value = copy(value)
 
                     try:
-                        member = [s for s in sub_space if hash(
-                            s) == hash(value)][0]
+                        member = next(filter(
+                            lambda s: hash(s) == hash(value),
+                            sub_space
+                        ))
                         _value.set_hash(hash(member))
-                    except IndexError:
+                    except StopIteration:
                         pass
                     finally:
                         value = _value
@@ -156,9 +178,16 @@ class ClassFunction:
             for arg in self.params:
                 arg.type_init()
 
-    def get_new_func(self, context, sub_space, instance=None):
-        result = ClassFunction(self.func, [], None,
-                               context, self.params, instance)
+    def get_new_func(
+        self,
+        params: GetSampleParameters,
+        sub_space: List[BasicSearchSpace],
+        instance=None,
+        class_path=None
+    ):
+        result = ClassFunction(
+            self.func, [], None, params, self.params, instance, class_path=class_path
+        )
         result.sub_space = sub_space
 
         return result
@@ -175,23 +204,27 @@ class ClassFunction:
                     new_args.append(args[args_index])
                     args_index += 1
                 else:
-                    new_args.append(arg.get_sample(
-                        context=self.context,
-                        sub_space=self.sub_space)
+                    new_args.append(
+                        arg.get_sample(
+                            params=self.sampler_params,
+                            sub_space=self.sub_space,
+                            path=self.class_path
+                        )
                     )
         return new_args
 
     def __call__(self, *args, **kwds):
-        inner_context = self.context
-        self.context = self.context.create_child(
-            f'called_{self.func.__name__}')
+        previous_params = self.sampler_params
+        self.sampler_params = self.sampler_params.create_child_context(
+            f'called_{self.func.__name__}'
+        )
 
         try:
             new_args = self.sample_params(*args, **kwds)
         except Exception as e:
             error = e
 
-        self.context = inner_context
+        self.sampler_params = previous_params
 
         try:
             raise error
@@ -207,8 +240,10 @@ class SpaceFactory(SearchSpace):
         sampler=None,
         ast=None,
         clean_asts=None,
-        layers=[]
+        layers=[],
+        path=None
     ) -> None:
+        path = _type.__name__ if path is None else path
 
         super().__init__(
             _type,
@@ -216,11 +251,12 @@ class SpaceFactory(SearchSpace):
             sampler,
             ast_constraint.AstRoot() if ast is None else ast,
             ast_constraint.AstRoot() if ast is None else clean_asts,
-            layers
+            layers,
+            path=path
         )
 
         self.type = _type
-        self._sub_space = {}
+        self._sub_space: Dict[str, BasicSearchSpace] = {}
         self.space_name = _type.__name__
 
         for name, member in inspect.getmembers(self.type):
@@ -228,6 +264,7 @@ class SpaceFactory(SearchSpace):
                 if isinstance(member.space, BasicSearchSpace):
                     self._sub_space[name] = copy(member.space)
                     self._sub_space[name].set_hash(hash(member.space))
+                    self._sub_space[name].path_space = f'path.{name}'
                     self._sub_space[name].layers_append(
                         visitors.EvalAstChecked(),
                         visitors.MemberAstModifierVisitor(self, name)
@@ -251,23 +288,30 @@ class SpaceFactory(SearchSpace):
     def __ast_result_filter__(self, result):
         return result
 
-    def __sampler__(self, domain, context: SamplerContext):
-        instance_context = context.create_child(f'{self.space_name}_members')
+    def __sampler__(self, domain, params: GetSampleParameters):
+        instance_params = params.create_child_context(
+            f'{self.space_name}_members'
+        )
         key_list = list(self._sub_space.values())
         class_func = self._metadata['__init__'].get_new_func(
-            instance_context, key_list)
+            instance_params, key_list, class_path=self.path_space
+        )
 
         class_instance = self.type(*class_func.sample_params())
-        class_instance.__instance_context__ = instance_context
+        class_instance.__instance_context__ = instance_params
 
         for name, class_func in self._metadata.items():
             if name == '__init__':
                 continue
 
-            setattr(class_instance, name,
-                    class_func.get_new_func(instance_context, key_list, class_instance))
+            setattr(
+                class_instance, name,
+                class_func.get_new_func(
+                    instance_params, key_list, class_instance, class_path=self.path_space
+                )
+            )
 
-        return class_instance, instance_context
+        return class_instance, instance_params.context
 
     def __ast_optimization__(self, ast_list):
 
@@ -296,19 +340,28 @@ class SpaceFactory(SearchSpace):
 
 
 class SelfSpace:
-    def __init__(self, distribute_like=None) -> None:
+    def __init__(self, distribute_like=None, path=None) -> None:
         self.self_space = None
         self.ast_cache = []
+        self.path_space = path
 
-    def get_sample(self, context=None, local_domain=None):
-        return self.self_space.get_sample()[0], context
+    def get_sample(self, params: GetSampleParameters) -> GetSampleResult:
+        self_params = GetSampleParameters(sampler=params.sampler)
+        result = self.self_space.get_sample(self_params)
+        return params.build_result(result.sample)
 
     def __getattr__(self, __name: str):
         return getattr(self.self_space, __name)
 
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        try:
+            setattr(self.self_space, __name, __value)
+        except AttributeError:
+            super().__setattr__(__name, __value)
+
     def __self_assign__(self, space):
         if self.self_space is None:
-            self.self_space = SpacesManager().get_space_by_type(space)()
+            self.self_space = SpacesManager().get_space_by_type(space)(path=self.path_space)
             for ast in self.ast_cache:
                 self.self_space = self.self_space.__ast_optimization__(ast)
 
@@ -325,8 +378,8 @@ class NoneSpace(BasicSearchSpace):
     def __init__(self) -> None:
         super().__init__(None, None)
 
-    def get_sample(self, context=None, local_domain=None):
-        return None, context
+    def get_sample(self, params: GetSampleParameters, local_domain=None):
+        return params.build_result(None)
 
     def __ast_optimization__(self, ast_list):
         return self
@@ -339,11 +392,12 @@ class UnionSpace(SearchSpace):
     def __init__(
         self,
         *domain,
-        distribute_like=UNIFORM,
+        distribute_like=BERNOULLI,
         sampler=None,
         ast=None,
         clean_asts=None,
-        layers=[]
+        layers=[],
+        path=None
     ) -> None:
 
         super().__init__(
@@ -353,12 +407,13 @@ class UnionSpace(SearchSpace):
             ast_constraint.AstRoot() if ast is None else ast,
             ast_constraint.AstRoot() if ast is None else clean_asts,
             layers=[visitors.TypeDomainModifierVisitor()] if len(
-                layers) == 0 else layers
+                layers) == 0 else layers,
+            path=path
         )
 
-    def get_sample(self, context=None, local_domain=None):
-        space, context = super().get_sample(context)
-        return space.get_sample(context)
+    def get_sample(self, params: GetSampleParameters) -> GetSampleResult:
+        result = super().get_sample(params)
+        return result.sample.get_sample(params)
 
         # try:
         # except RecursionError:
@@ -369,9 +424,12 @@ class UnionSpace(SearchSpace):
         #     else:
         #         raise RecursionErrorSpaceDefinition
 
-    def __call__(self, *args, **kwds):
+    def __call__(self, *args, path=None, **kwds):
+        path = 'union_space' if path is None else path + '.union'
+        self.space_name = path
         self.initial_domain = CategoricalDomain(
-            [space(*args, **kwds) for space in self.initial_domain.list]
+            [space(*args, path=f'{path}._type_{i}', **kwds)
+             for i, space in enumerate(self.initial_domain.list)]
         )
 
         return self
@@ -396,7 +454,7 @@ class UnionSpace(SearchSpace):
             except AttributeError:
                 pass
 
-    def __check_sample__(self, sample, ast_result, context):
+    def __check_sample__(self, sample, ast_result, params):
         return
 
     def __copy__(self):
@@ -406,7 +464,8 @@ class UnionSpace(SearchSpace):
             sampler=None,
             ast=ast_constraint.AstRoot(copy(self.ast_constraint.asts)),
             clean_asts=ast_constraint.AstRoot(copy(self._clean_asts.asts)),
-            layers=copy(self.visitor_layers)
+            layers=copy(self.visitor_layers),
+            path=self.space_name
         )
 
         result.initial_domain = copy(self.initial_domain)
